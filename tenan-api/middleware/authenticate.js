5e62d7981d179c7579b42c7a867320184714484c
Muhammad Ferdiansyah <ferdimuh1405@gmail.com>
2023-06-15 19:17:17 +0700

    Finalize Project

--- tenan-api/middleware/authenticate.js
+++ tenan-api/middleware/authenticate.js
@@ -123,45 +123,46 @@ const optionalAuthenticateAccessToken = (req, res, next) => {
   const token = authHeader && authHeader.split(' ')[1];
   if (token == null) {
     next();
-  }
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded) {
-    if (err) {
-      if (err.name === 'TokenExpiredError') {
-        // Handle the expired token error
+  } else {
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded) {
+      if (err) {
+        if (err.name === 'TokenExpiredError') {
+          // Handle the expired token error
+          return res.status(401).send({
+            code: '401',
+            status: 'Unauthorized',
+            errors: {
+              message: 'Token expired. Please get new access token',
+            },
+          });
+        } else if (err.name === 'JsonWebTokenError') {
+          // Handle the invalid token error
+          return res.status(401).send({
+            code: '401',
+            status: 'Unauthorized',
+            errors: {
+              message: 'Token invalid',
+            },
+          });
+        }
+
+        console.log(err);
         return res.status(401).send({
           code: '401',
           status: 'Unauthorized',
           errors: {
-            message: 'Token expired. Please get new access token',
-          },
-        });
-      } else if (err.name === 'JsonWebTokenError') {
-        // Handle the invalid token error
-        return res.status(401).send({
-          code: '401',
-          status: 'Unauthorized',
-          errors: {
-            message: 'Token invalid',
+            message: 'Unknown Error',
           },
         });
       }
 
-      console.log(err);
-      return res.status(401).send({
-        code: '401',
-        status: 'Unauthorized',
-        errors: {
-          message: 'Unknown Error',
-        },
-      });
-    }
-
-    req.email = decoded.email;
-    req.name = decoded.name;
-    req.user_id = decoded.user_id;
-    req.created_at = decoded.created_at;
-    next();
-  });
+      req.email = decoded.email;
+      req.name = decoded.name;
+      req.user_id = decoded.user_id;
+      req.created_at = decoded.created_at;
+      next();
+    });
+  }
 };
 
 
